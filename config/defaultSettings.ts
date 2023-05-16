573f25fd1d6d6cd9d7e692bd3b056fef95c94b73
CodeJuzi <d1741530592@163.com>
2023-05-16 17:01:30 +0800

    apperence update

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'User Center Backend',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
