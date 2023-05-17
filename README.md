ca594f8a3970384d5f5629da0aacb82e3cd4fe15
CodeJuzi <d1741530592@163.com>
2023-05-17 21:32:17 +0800

    update: readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # 用户中心前端
+
+## 如何上线？
+
+1. 打包项目
+   
+   ```sh
+   npm run build
+   ```
+   
+1. 将dist目录上传服务器
+   
+1. 上传Dockerfile和`docker`文件夹一起上传至和dist同级的目录
+   
+1. 构建镜像
+   
+   ```sh
+   docker build -t user-center-frontend:0.0.1 .
+   ```
+
+5. 启动镜像
+	```sh
+	docker run -p 80:80 -d user-center-frontend:0.0.1
+	```
+
+6. 访问ip即可
+
+
+
--- src/app.tsx
+++ src/app.tsx
@@ -124,7 +124,8 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://8.130.102.239:8080':'http://localhost:8101',
+  // todo
+  baseURL: process.env.NODE_ENV === 'production' ? '后端请求地址' : 'http://localhost:8101',
   withCredentials: true,
   ...errorConfig,
 };
